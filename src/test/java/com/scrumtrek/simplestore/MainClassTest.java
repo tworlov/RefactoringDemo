5dda403ce1e80abe44fb425077f8ab6e96d0f6f0
tworlov <tworlov@gmail.com>
2015-09-22 12:16:47 +0400

    check PriceCodes

---
+++ src/test/java/com/scrumtrek/simplestore/MainClassTest.java
@@ -0,0 +1,12 @@
+package com.scrumtrek.simplestore;
+
+import org.junit.Test;
+
+public class MainClassTest {
+
+	@Test
+	public void shouldWhen() {
+
+	}
+
+}
---
+++ src/test/java/com/scrumtrek/simplestore/MovieTest.java
@@ -0,0 +1,37 @@
+package com.scrumtrek.simplestore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MovieTest {
+
+	@Test
+	public void shouldSetTitleCinderellaWhenCreateMovieCinderella() {
+
+		// given
+
+		// when
+		Movie cinderella = new Movie("Cinderella", null);
+
+		// then
+		assertEquals("Cinderella", cinderella.getTitle());
+
+	}
+	
+	@Test
+	public void shouldChangePriceCodeWhenSetNewPriceCode() {
+
+		// given
+		Movie cinderella = new Movie("Cinderella", PriceCodes.Regular);
+
+
+		// when
+		cinderella.setPriceCode(PriceCodes.Childrens);
+
+		// then
+		assertEquals(PriceCodes.Childrens, cinderella.getPriceCode());
+
+	}
+
+}
