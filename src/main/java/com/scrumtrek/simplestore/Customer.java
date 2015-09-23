2d661ce4fad30623cc0d33149c1d39d93f1cab66
tworlov <tworlov@gmail.com>
2015-09-23 10:33:29 +0400

    Customer

--- src/main/java/com/scrumtrek/simplestore/Customer.java
+++ src/main/java/com/scrumtrek/simplestore/Customer.java
@@ -15,51 +15,46 @@ public class Customer {
 		return m_Name;
 	}
 
-
-	public void addRental(Rental arg){
+	public void addRental(Rental arg) {
 		m_Rentals.add(arg);
 	}
 
-	public String Statement()
-	{
+	public String Statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-				
+
 		String result = "Rental record for " + m_Name + "\n";
-		
-		for(Rental each: m_Rentals) {
+
+		for (Rental each : m_Rentals) {
 			double thisAmount = 0;
-			
+
 			// Determine amounts for each line
-			switch(each.getMovie().getPriceCode()) {
-				case Regular:
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-					{
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					}
-					break;
-	
-				case NewRelease:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-	
-				case Childrens:
-					thisAmount += 1.5;
-					if (each.getDaysRented() > 3)
-					{
-						thisAmount = (each.getDaysRented() - 3) * 1.5;
-					}
-					break;
+			switch (each.getMovie().getPriceCode()) {
+			case Regular:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2) {
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				}
+				break;
+
+			case NewRelease:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+
+			case Childrens:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3) {
+					thisAmount = (each.getDaysRented() - 3) * 1.5;
+				}
+				break;
 			}
 
 			// Add frequent renter points
 			frequentRenterPoints++;
 
 			// Add bonus for a two-day new-release rental
-			if ((each.getMovie().getPriceCode() == PriceCodes.NewRelease) && (each.getDaysRented() > 1))
-			{
-				frequentRenterPoints ++;
+			if ((each.getMovie().getPriceCode() == PriceCodes.NewRelease) && (each.getDaysRented() > 1)) {
+				frequentRenterPoints++;
 			}
 
 			// Show figures for this rental
@@ -73,4 +68,3 @@ public class Customer {
 		return result;
 	}
 }
-
--- src/test/java/com/scrumtrek/simplestore/MainClassTest.java
+++ src/test/java/com/scrumtrek/simplestore/MainClassTest.java
@@ -6,7 +6,7 @@ public class MainClassTest {
 
 	@Test
 	public void shouldWhen() {
-
+		
 	}
 
 }
